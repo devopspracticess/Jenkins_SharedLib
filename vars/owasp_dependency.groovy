33c66c6394c5b3e68245623cd542dfb0760f25fb
devopspracticess <vasantbhosale028@gmail.com>
2026-02-01 11:28:40 +0530

    Update owasp_dependency.groovy

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -10,6 +10,8 @@ def call() {
           --out dependency-check-report
           --noupdate
           --disableAssembly
+          --disableNodeAudit
+          --disableOSSIndex
         """
       )
     }
